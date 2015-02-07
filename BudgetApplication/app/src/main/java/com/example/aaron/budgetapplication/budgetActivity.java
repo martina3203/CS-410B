3b348d9664a2cdf0e01146e9426851677fa10bbe
danielj11 <danielj11@mycu.concord.edu>
2015-02-07 18:52:57 -0500

    Saving User Input Implemented
    I figured out how to save text inputted in an editText.  At the moment,
    it only works for the first row of entries.  If you press the save
    button after inputting values, you should get a notification at the
    bottom of the screen.

--- BudgetApplication/app/src/main/java/com/example/aaron/budgetapplication/budgetActivity.java
+++ BudgetApplication/app/src/main/java/com/example/aaron/budgetapplication/budgetActivity.java
@@ -4,14 +4,26 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class budgetActivity extends ActionBarActivity {
+
+    private Button saveButton; //Button that saves input when pressed
+    private EditText entryEditText1; //entry name input field
+    private EditText entryCostEdit1; //entry cost input field
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_budget);
+
+        saveButton = (Button) findViewById(R.id.saveButton);
+        entryEditText1 = (EditText) findViewById(R.id.entryTextEdit1);
+        entryCostEdit1 = (EditText) findViewById(R.id.entryCostEdit1);
     }
 
 
@@ -35,5 +47,25 @@ public class budgetActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onSaveClick(View view) {
+
+        //gets text from entryEditText1
+        String entryName = String.valueOf(entryEditText1.getText());
+
+        //gets text from costEditText1
+        String entryPriceStr = String.valueOf(entryCostEdit1.getText());
+
+        //changes string into a float since I can only read in strings
+        Float f = Float.parseFloat(entryPriceStr);
+
+        //strings to be outputted
+        String testString = "The entry name was " + entryName;
+        String testString2 = "The entry price was " + f;
+
+        //displays notifications on the screen just to shoe that this is working
+        Toast.makeText(this, testString,Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, testString2,Toast.LENGTH_SHORT).show();
     }
 }

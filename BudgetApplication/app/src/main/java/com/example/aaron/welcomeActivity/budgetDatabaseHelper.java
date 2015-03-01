4250a0eaf0a4cb932bc4f9901701ccee52f13639
Aaron Martin <martina3203@gmail.com>
2015-03-01 17:29:43 -0500

    Updated methods to add and remove subtables.

--- BudgetApplication/app/src/main/java/com/example/aaron/welcomeActivity/budgetDatabaseHelper.java
+++ BudgetApplication/app/src/main/java/com/example/aaron/welcomeActivity/budgetDatabaseHelper.java
@@ -27,6 +27,8 @@ public class budgetDatabaseHelper extends SQLiteOpenHelper {
     private static final String COLUMN_EXPENSE_MAX_COST = "EXPENSE_MAX_COST";
 
     //This is the command that is executed to create a database
+    //The formatting is basically like this: create table TABLE_NAME ( COLUMN_NAME DATA_TYPE, ...);
+    //Accepted Data types are Text, Integers, Reals.
     private static final String DATABASE_CREATE = "create table "
             + BUDGET_TABLE_NAME + "(" + COLUMN_ID
             + " integer primary key autoincrement, " + COLUMN_BUDGET_NAME
@@ -37,18 +39,34 @@ public class budgetDatabaseHelper extends SQLiteOpenHelper {
         super(context,DATABASE_NAME,null,DATABASE_VERSION);
     }
 
+    //Called on creation
     public void onCreate(SQLiteDatabase database)
     {
         //Carries out the creation of such a table
         database.execSQL(DATABASE_CREATE);
     }
 
-    public void addSubTable(String newTableName)
+    //Adds a subtable of expenses that corresponds to a budget
+    public void addExpenseTable(String newTableName,SQLiteDatabase database)
     {
-        //String command = "create table " + newTableName + "(" + COLUMN_ID + ")";
+        //This is the string for execution via SQL.execSQL
+        String command = "create table " + newTableName +
+                "(" + COLUMN_ID + " integer primary key autoincrement, " +
+                COLUMN_EXPENSE_NAME  + "text, " + COLUMN_EXPENSE_PRIORITY + "integer," +
+                COLUMN_EXPENSE_COST + "real, " + COLUMN_EXPENSE_MAX_COST + "real)";
+        database.execSQL(command);
+
     }
+
+    //Removes an existing subtable of the expenses that corresponds to a budget
+    public void removeSubTable(String tableName, SQLiteDatabase database)
+    {
+        String command = "DROP TABLE " + DATABASE_NAME + "." + tableName;
+        database.execSQL(command);
+    }
+
     public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion)
     {
-        //This will handle if we want to delete stuff
+        //This will handle if we want to delete the database and upgrade
     }
 }

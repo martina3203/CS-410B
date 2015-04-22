f69aadf079bb9839e6c941ee28d9d9a2448f7fa6
danielj11 <danielj11@mycu.concord.edu>
2015-04-22 19:36:54 -0400

    Minor Changes

--- BudgetApplication/app/src/main/java/com/example/aaron/welcomeActivity/budgetOverviewActivity.java
+++ BudgetApplication/app/src/main/java/com/example/aaron/welcomeActivity/budgetOverviewActivity.java
@@ -3,8 +3,6 @@ package com.example.aaron.welcomeActivity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -137,7 +135,8 @@ public class budgetOverviewActivity extends ActionBarActivity {
                 parent.getChildAt(position).setBackgroundColor(Color.LTGRAY);
                 //Revert the previous color
                 if (previousListPosition != -1 && previousListPosition != position){
-                    parent.getChildAt(previousListPosition).setBackgroundColor(Color.parseColor(mainActivity.BACKGROUND_COLOR));
+                    parent.getChildAt(previousListPosition).
+                            setBackgroundColor(Color.parseColor(mainActivity.BACKGROUND_COLOR));
                 }
                 //Update previous saved position so that we can revert it later
                 previousListPosition = position;
--- BudgetApplication/app/src/main/java/com/example/aaron/welcomeActivity/editItemActivity.java
+++ BudgetApplication/app/src/main/java/com/example/aaron/welcomeActivity/editItemActivity.java
@@ -4,8 +4,6 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -61,6 +59,7 @@ public class editItemActivity extends ActionBarActivity{
         dropdown.setAdapter(priorityAdapter);
 
         theDatabase = new DatabaseAccess(getApplicationContext());
+        builder = new AlertDialog.Builder(this);
 
         //Get intent from item overview
         Intent receivedIntent = this.getIntent();
@@ -153,25 +152,4 @@ public class editItemActivity extends ActionBarActivity{
         startActivity(newIntent);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_budget, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
